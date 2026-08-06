b66fb361a83406927ba46effc0699558208236ed
Workfall <workfall@Workfalls-Mac-mini-3.local>
2026-08-06 12:37:23 +0530

    my first commit

---
+++ tests/login.spec.js
@@ -0,0 +1,30 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '../pages/login.page';
+import { DashboardPage } from '../pages/dashboard.page';
+import { getFirstUser } from '../utils/jsonReader';
+
+test.describe('Login Module', () => {
+
+    test('Login with valid credentials', async ({ page }) => {
+
+        const loginPage = new LoginPage(page);
+        const dashboardPage = new DashboardPage(page);
+
+        const user = getFirstUser();
+
+        await loginPage.goto();
+
+        await page.pause;
+        
+        await loginPage.login(
+            user.email,
+            user.password
+        );
+
+        await dashboardPage.openProjectManagement();
+
+        await dashboardPage.verifyProjectPage();
+
+    });
+
+});
