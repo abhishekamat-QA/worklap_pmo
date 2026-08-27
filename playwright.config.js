8b5eac73be178474f6cfe9a569cda27f41ace7c6
Workfall <workfall@Workfalls-Mac-mini-3.local>
2026-08-27 12:20:46 +0530

    27 Aug 2026 @12.30

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['allure-playwright']],
+  //reporter: [['html'], ['allure-playwright']],
+  reporter: [
+    ['list'],
+    [
+      'allure-playwright',
+      {
+        resultsDir: 'allure-results',
+        detail: true,
+        suiteTitle: true
+      }
+    ]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
